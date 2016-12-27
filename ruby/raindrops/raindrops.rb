4b66c98cc1f237af6b908e4aa2f9bf5cd492296b
Will Ockelmann-Wagner <william.lee.wagner@gmail.com>
2016-12-26 16:25:02 -0800

    solve ruby raindrops

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,19 @@
+module BookKeeping
+  VERSION = 3
+end
+
+class Raindrops
+  def self.convert(number)
+    answer = ""
+
+    answer += "Pling" if factor_of(number, 3)
+    answer += "Plang" if factor_of(number, 5)
+    answer += "Plong" if factor_of(number, 7)
+
+    answer == "" ? number.to_s : answer
+  end
+
+  def self.factor_of(number, factor)
+    (number % factor).zero?
+  end
+end
