d9f745f960e3877f5b4c6484a8e3efe9d69a9b35
Will Ockelmann-Wagner <william.lee.wagner@gmail.com>
2016-12-27 01:04:23 -0800

    solve elixir pangram

--- elixir/pangram/pangram.exs
+++ elixir/pangram/pangram.exs
@@ -13,6 +13,28 @@ defmodule Pangram do
   """
 
   @spec pangram?(String.t) :: boolean
+  def pangram?(""), do: false
   def pangram?(sentence) do
+    sentence
+    |> String.downcase
+    # Remote all non-letter characters.
+    |> String.replace(~r/[^a-z]/, "")
+    |> String.to_charlist
+    |> Enum.sort
+    |> pangram_check
+  end
+
+  defp pangram_check(text), do: pangram_check(text, ?a - 1)
+  # If all letters are gone, and we saw z, success.
+  defp pangram_check([], ?z), do: true
+  # If all letters are gone before z, fail.
+  defp pangram_check([], _), do: false
+  defp pangram_check([char | rest], last_char) do
+    # If the character is the last one again, or the next one, all good.
+    if last_char == char - 1 || last_char == char do
+      pangram_check(rest, char)
+    else
+      false
+    end
   end
 end
---
+++ ruby/pangram/pangram.rb
@@ -0,0 +1,20 @@
+# Determine if a sentence is a pangram. A pangram
+# is a sentence using every letter of the alphabet at least once.
+# The best known English pangram is
+# "The quick brown fox jumps over the lazy dog."
+
+# The alphabet used is ASCII, and case insensitive, from 'a' to 'z'
+# inclusively.
+
+module BookKeeping
+  VERSION = 3
+end
+
+class Pangram
+  def self.pangram?(_text)
+
+  end
+
+  def self.alphabet
+  end
+end
