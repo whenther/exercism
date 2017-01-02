c1818215695b54435a6885c037427188550c46e4
Will Ockelmann-Wagner <william.lee.wagner@gmail.com>
2017-01-02 13:55:57 -0800

    solve elixir triangle

--- elixir/triangle/triangle.exs
+++ elixir/triangle/triangle.exs
@@ -6,6 +6,17 @@ defmodule Triangle do
   """
   @spec kind(number, number, number) :: { :ok, kind } | { :error, String.t }
   def kind(a, b, c) do
-
+    [a, b, c]
+    |> Enum.sort
+    |> Enum.reverse
+    |> ordered_check
   end
+
+  defp ordered_check([a, b, c]) when a <= 0 or b <= 0 or c <= 0, do: {:error, "all side lengths must be positive"}
+  # The hint seems to suggest adding this check, but that fails a test, so I dunno.
+  # defp ordered_check([a, b, c]) when a == b + c, do: {:ok, :degenerate}
+  defp ordered_check([a, b, c]) when a >= b + c, do: {:error, "side lengths violate triangle inequality"}
+  defp ordered_check([a, a, a]), do: {:ok, :equilateral}
+  defp ordered_check([a, a, _c]), do: {:ok, :isosceles}
+  defp ordered_check([_a, _b, _c]), do: {:ok, :scalene}
 end
